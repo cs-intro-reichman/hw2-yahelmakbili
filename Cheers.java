84e3f2d59dda4633b20f9337fcca7219dde9a828
yahelmakbili <yahel.makbili@post.runi.ac.il>
2025-11-07 20:58:00 +0200

    submit

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,24 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                // קלט מילה ומספר
+                String word = args[0];
+                int N = Integer.parseInt(args[1]);
+                String vowels = "AEOIU";
+                int i = 0;
+                // מדפיסה כל אות
+                while (i < word.length()) {
+                        char o = word.charAt(i);
+                        if (vowels.indexOf(o) == -1) {
+                                System.out.println("Give me a " + o + ":" + o + "!");
+                        } else {
+                                System.out.println("Give me an " + o + ":" + o + "!");
+                        }
+                        i++;
+                }
+                int t = 0;
+                for (t = 0; t < N; t++) {
+                        System.out.println(word + "!!!");
+                }
+
         }
 }
