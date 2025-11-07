dc2a214565f5368fd5bd570fd85a32b44a3e5014
yahelmakbili <yahel.makbili@post.runi.ac.il>
2025-11-07 22:03:59 +0200

    submit

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,28 @@
 // Computes an approximation of PI.
 public class CalcPi {
-	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	public static void main(String[] args) {
+		// קלט מהמשתמש
+		int N = Integer.parseInt(args[0]);
+		double pi = Math.PI;
+		double numerator = 1;
+		double denominator = 1;
+		double piApproximated = 0;
+		int i = 0;
+		// חישוב לפי הנוסחה
+
+		for (i = 0; i < N; i++) {
+			if (i % 2 == 0) {
+				piApproximated += (numerator / denominator);
+			} else {
+				piApproximated -= (numerator / denominator);
+			}
+			// נוסיף למכנה ונגדיל את איי
+			denominator += 2;
+			i++;
+		}
+		piApproximated *= 4;
+		System.out.println("pi according to Java: " + pi);
+		System.out.println("pi, appriximated:     " + piApproximated);
+
 	}
 }
